5fb221e65689edcc6e9c7a17c22246190913c2bf
mamz <mmz1995@sina.com>
2021-11-04 22:59:06 +0800

    feat(parent): note for task.

--- webmagic-copy-core/src/main/java/com/mmz/webmagic/copy/Task.java
+++ webmagic-copy-core/src/main/java/com/mmz/webmagic/copy/Task.java
@@ -7,7 +7,16 @@ package com.mmz.webmagic.copy;
  **/
 public interface Task {
 
+    /**
+     * unique id for a task.
+     *
+     * @return uuid
+     */
     public String getUUID();
 
+    /**
+     * site of a task
+     * @return site
+     */
     public Site getSite();
 }
